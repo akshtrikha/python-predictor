96374e4f22c6526e5bb45a898095ef813cef31d9
Akshit Trikha <akshittrikha01@gmail.com>
2021-06-06 21:04:26 +0000

    upd: Add run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=akshtrikha/python-predictor
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --image=$dockerpath predictor-app --port=80
+# --generator flag is added to ensure expected behaviour
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward predictor-app 8000:80
 
